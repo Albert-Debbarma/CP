3accfc87eca61a757e29c674aaf0e56a768ace9d
Albert-Debbarma <96245890+Albert-Debbarma@users.noreply.github.com>
2022-02-02 22:50:22 +0530

    Create A_THeatre_square.cpp

---
+++ CF/A_THeatre_square.cpp
@@ -0,0 +1,38 @@
+    #include<iostream>
+     #include <string>
+    #include <vector>
+    #include <algorithm>
+    #include <sstream>
+    #include <queue>
+    #include <deque>
+    #include <bitset>
+    #include <iterator>
+    #include <list>
+    #include <stack>
+    #include <map>
+    #include <set>
+    #include <functional>
+    #include <numeric>
+    #include <utility>
+    #include <limits>
+    #include <time.h>
+    #include <math.h>
+    #include <stdio.h>
+    #include <string.h>
+    #include <stdlib.h>
+    #include <assert.h>
+    using namespace std ;
+    #define ar array
+    #define ll long long
+    #define ld long double
+    int main(){
+     ld n,m,a;
+    cin>>n>>m>>a;
+    ll x =ceil(m/a);
+    ll  y= ceil(n/a);
+    cout<<x*y<<endl;
+     
+    return 0 ;
+    }
+
+→Judgement Protocol    
